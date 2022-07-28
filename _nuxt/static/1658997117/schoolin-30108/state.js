764cab4fe2a5c79b60732ac155c13dc19bee1980
葉奕緯 <frank@yeyiweideMacBook-Air.local>
2022-07-28 16:35:03 +0800

    add text

---
+++ _nuxt/static/1658997117/schoolin-30108/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Factivity\u002F2022\u002Fprivate\u002F_nuxt\u002Fstatic\u002F1658997117",layout:"default",error:a,serverRendered:true,routePath:"\u002Fschoolin-30108",config:{_app:{basePath:"\u002Factivity\u002F2022\u002Fprivate\u002F",assetsPath:"\u002Factivity\u002F2022\u002Fprivate\u002F_nuxt\u002F",cdnURL:a}}}}(null));
